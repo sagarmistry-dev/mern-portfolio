32ccc8a0932954a9d2ed62804506186ab5e23047
sagarmistry-dev <sagarmistry453@gmail.com>
2025-11-28 01:11:09 +0530

    fix: update API_BASE_URL for production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,3 +1,3 @@
 // central place for the backend URL
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://mern-portfolio-prtq.onrender.com/api";
 export default API_BASE_URL;
